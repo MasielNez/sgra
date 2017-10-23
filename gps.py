1117e838f823378a147f5debd8b1ea737d6a2d19
Masiel Nunez <masielnez1@gmail.com>
2017-10-23 02:49:48 +0000

    Include HTTP request in gps.py

--- gps.py
+++ gps.py
@@ -18,8 +18,6 @@ ser = serial.Serial(
 )
 
 while 1:
-	subprocess.call("sudo poff fona", shell=True)
-	time.sleep(1)
 	ser = serial.Serial(
 		port='/dev/ttyAMA0',
 		baudrate = 115200,
@@ -29,16 +27,19 @@ while 1:
 		timeout=1
 	)
 	time.sleep(1)
+	ser.write('AT+CMGF=1\r')
+	ser.write('AT+CGATT=1\r')
+	ser.write('AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\r')
+	ser.write('AT+SAPBR=3,1,\"APN\",\"data.viva.do\"\r')
+	ser.write('AT+SAPBR=1,1\r')
 	ser.write('AT+CGNSINF\r')
 	counter=0
 	x=ser.readline()
-	while ('+CGNSINF:' not in x) and (counter < 5):
+	while('+CGNSINF:' not in x) and (counter < 5):
 		x=ser.readline()
 		counter+=1
-	subprocess.call("sudo pon fona", shell=True)
-	time.sleep(3)
 	data = x.split(',')
-	update = urllib2.urlopen("http://api.dominitec.com/update-gps-info.php?device_id=" + device_id + "&password=" + password + "&latitude=" + data[3] + "&longitude=" + data[4] + "&altitude=" + data[5] + "&utctime=" + data[2] + "&speed=" + data[6] + "&course=" + data[7]).read()
-	time.sleep(1)
-	print update
-	
+	ser.write('AT+HTTPINIT\r')
+	ser.write('AT+HTTPPARA=\"CID\",1\r')
+	ser.write('AT+HTTPPARA=\"URL\",\"http://api.dominitec.com/update-gps-info.php?device_id=' + device_id + '&password=' + password + '&latitude=' + data[3] + '&longitude=' + data[4] + '&altitude=' + data[5] + '&utctime=' + data[2] + '&speed=' + data[6] + '&course=' + data[7] + '\"\r')
+	print ser.readline()
